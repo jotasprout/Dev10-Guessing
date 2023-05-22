64d2152e60ad2ce156aa52fbb44854f0309bcde0
Jay Sprout <jotasprout@gmail.com>
2023-05-22 16:34:47 -0400

    building core

---
+++ coin-flip.js
@@ -0,0 +1,9 @@
+/*
+
+When either button is clicked, call a function that takes the guess (heads or tails) as an argument
+
+generates a random number to represent the coin flip
+
+display a message about the result of the flip.
+
+*/
---
+++ high-low.js
@@ -0,0 +1,15 @@
+/*
+
+The application prompts the user for a maximum number.
+
+The application validates the user input and does not allow invalid entries (negative numbers, 0, or non-numbers), re-prompting the user if an invalid entry is provided.
+
+If the user provides a decimal number, the application rounds it.
+
+The application selects a random number between 1 and N (where N is the user-provided maximum number).
+
+If the guess is not a number, display a message: "That is not a number!"
+
+If the guess is out of range (1 to N), display a message: "That number is not in range, try again."
+
+*/

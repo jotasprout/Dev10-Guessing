c938bc2ec27da4f7321fa0ad7bd47f3e6ccdbf4e
Jay Sprout <jotasprout@gmail.com>
2023-05-24 22:11:14 -0400

    moving comments

--- enhanced.js
+++ enhanced.js
@@ -35,6 +35,13 @@ The prompt should be in a loop with validation, as demonstrated previously in th
   
 The application validates the user input and does not allow invalid entries (negative numbers, 0, or non-numbers), re-prompting the user if an invalid entry is provided.
 
+If the guess is not a number, display a message: "That is not a number!"
+If the guess is out of range (1 to N), display a message: "That number is not in range, try again."
+Grading Criteria
+The application prevents the user from guessing a non-number and displays an appropriate error message.
+The application prevents the user from guessing a number less than 1 and displays an appropriate error message.
+The application prevents the user from guessing a number greater than N and displays an appropriate error message.
+
 When a valid number is inputted, change the content of the instructions to specify guesses between 1 and N (where N is the user-provided maximum number).
 
 The application selects a random number between 1 and N (where N is the user-provided maximum number).
